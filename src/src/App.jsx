512d1b97d29c6adb92dd452c8be3385ec87c3864
wbgl-blip <wbgl@live.com>
2025-12-13 17:46:30 -0500

    Delete src/src/App.jsx

--- src/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'system-ui' }}>
-      <h1>🍺 Virtual Kings Cup</h1>
-    </div>
-  )
-}
