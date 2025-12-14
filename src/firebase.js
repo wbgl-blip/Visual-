5e15c2fcb4d16b96d0147c81b62cbdefb3be4ec8
wbgl-blip <wbgl@live.com>
2025-12-14 11:00:49 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyAl-yHucmRzi1YVewe0dINlejxRzfgfG0c",
+  authDomain: "kad-kings.firebaseapp.com",
+  projectId: "kad-kings",
+  storageBucket: "kad-kings.appspot.com",
+  messagingSenderId: "500155881950",
+  appId: "1:500155881950:web:c53d5a53a98a49bf5ce54f"
 };
 
 const app = initializeApp(firebaseConfig);
