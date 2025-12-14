fe10d9a3aa218d1f6d40d57bc85c09d5e19d507b
wbgl-blip <wbgl@live.com>
2025-12-14 05:16:54 -0500

    Use environment variable for Firebase API key

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "kad-kings.firebaseapp.com",
   databaseURL: "https://kad-kings-default-rtdb.firebaseio.com",
   projectId: "kad-kings",
   storageBucket: "kad-kings.appspot.com",
   messagingSenderId: "XXXX",
-  appId: "XXXX"
+  appId: "XXXX",
 };
 
 const app = initializeApp(firebaseConfig);
