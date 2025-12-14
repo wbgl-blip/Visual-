c177b80036ff9f84065866111a6fc34754ba83a6
wbgl-blip <wbgl@live.com>
2025-12-14 04:14:08 -0500

    Add Firebase initialization and database setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "YOUR_DB_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
